9fc2f5d7d261d6882d7848f29a61b45cc4ecefb2
Aman Pratap Singh <120871048+Aman07122003@users.noreply.github.com>
2025-08-17 20:11:29 +0530

    Set size

--- src/components/Stats/LeetCodeStats.jsx
+++ src/components/Stats/LeetCodeStats.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const LeetCodeStats = ({ username = 'Aman07122003' }) => {
   return (
-    <div className="mx-auto p-3 w-full bg-black/40 md:w-1/2 backdrop-blur-md border border-white/20 rounded-2xl shadow-lg transition-transform duration-300 hover:scale-[1.02]">
+    <div className="mx-auto p-3 w-full bg-black/40 md:w-2/3 lg:w-1/2 backdrop-blur-md border border-white/20 rounded-2xl shadow-lg transition-transform duration-300 hover:scale-[1.02]">
       {/* Header */}
       <div className="flex items-center justify-between mb-4">
-        <h2 className="md:text-lg font-semibold text-white">LeetCode Stats</h2>
+        <h2 className="text-sm md:text-lg font-semibold text-white">LeetCode Stats</h2>
         <a
           href={`https://leetcode.com/${username}`}
           target="_blank"
@@ -16,18 +16,13 @@ const LeetCodeStats = ({ username = 'Aman07122003' }) => {
         </a>
       </div>
 
-        {/* Left: Stats iframe */}
-        <div className=" overflow-hidden rounded-xl border border-white/10 shadow-inner">
-          <iframe
-            src={`https://leetcard.jacoblin.cool/${username}?theme=dark`}
-            style={{
-              width: '400px',
-              height: '150px',
-              border: 'none'
-            }}
-            title={`LeetCode Stats for ${username}`}
-          />
-        </div>
+      {/* Stats iframe */}
+      <iframe
+          src={`https://leetcard.jacoblin.cool/${username}?theme=dark`}
+          className="w-full h-[180px] md:h-[200px] lg:h-[220px]"
+          style={{ border: 'none' }}
+          title={`LeetCode Stats for ${username}`}
+        />
     </div>
   );
 };
